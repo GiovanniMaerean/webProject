e90fa0bb592817db058d9f76241e6fbc494d51ff
Giovanni <giovannimaerean@gmail.com>
2024-03-20 12:28:33 +0100

    Create user model Commit

--- steamApp/models.py
+++ steamApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Model):
+    steamID = models.IntegerField(primary_key=True)
+    realName = models.CharField(max_length=1000)
+    personaName = models.CharField(max_length=1000)
+    profileUrl = models.URLField()
+    countryCode = models.CharField(max_length=10)
+    timeCreated = models.DateTimeField(auto_now_add=True)
+    friends = models.ManyToManyField('self', symmetrical=True, related_name='friends')
+    
