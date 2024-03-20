33bf2c1ab98f6f1fbfb46e9eadb8b07206f34e98
Giovanni <giovannimaerean@gmail.com>
2024-03-20 13:25:36 +0100

    Register models commit

--- steamApp/models.py
+++ steamApp/models.py
@@ -4,18 +4,18 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    steamID = models.IntegerField(primary_key=True, null=True)
+    steamID = models.IntegerField(primary_key=True)
     realName = models.CharField(max_length=1000)
     personaName = models.CharField(max_length=1000)
     profileUrl = models.URLField()
     countryCode = models.CharField(max_length=10)
     timeCreated = models.DateTimeField(auto_now_add=True)
-    friends = models.ManyToManyField('self', symmetrical=True, related_name='friends')
-    products = models.ManyToManyField('Product', symmetrical=True, related_name='products')
+    friends = models.ManyToManyField('self', symmetrical=True, null=True, blank=True)
+    products = models.ManyToManyField('Product', symmetrical=True, null=True, blank=True)
 
 
 class Product(models.Model):
-    appId = models.IntegerField(primary_key=True, null=True)
+    appId = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200)
     type = models.CharField(max_length=100)
     age = models.IntegerField(null=True)
@@ -28,5 +28,5 @@ class Product(models.Model):
     discount = models.FloatField(null=True)
     languages = models.CharField(max_length=300)
     owners = models.IntegerField(null=True)
-    users = models.ManyToManyField('User', symmetrical=True, related_name='users')
+    users = models.ManyToManyField('User', symmetrical=True, blank=True, null=True)
     description = models.TextField()
