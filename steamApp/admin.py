54e7c74433b4aac65541eefa61d97e6a86b87fc7
Giovanni <giovannimaerean@gmail.com>
2024-04-09 01:51:31 +0200

    define achievement model commit

--- steamApp/admin.py
+++ steamApp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import SteamUser, Product, Developer, Publisher
+from .models import SteamUser, Product, Developer, Publisher, Achievement
 
 # Register your models here.
 admin.site.register(SteamUser)
 admin.site.register(Product)
 admin.site.register(Developer)
 admin.site.register(Publisher)
+admin.site.register(Achievement)
