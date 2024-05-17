dcb43f38482b16691e21c7bb0c52ba3d891bb5a2
Giovanni <giovannimaerean@gmail.com>
2024-05-18 01:20:20 +0200

    Modify and Delete test

--- steamApp/tests.py
+++ steamApp/tests.py
@@ -253,7 +253,6 @@ class ModifySteamUserTestCase(TestCase):
         self.assertEqual(self.steam_user.realName, 'Modified Name')
         self.assertEqual(self.steam_user.steamID, 5678)
 
-
     def test_steam_user_modify_invalid_form(self):
         self.client.login(username='testuser', password='12345')
         url = reverse('modifySteamUser', kwargs={'id': self.steam_user.id})
@@ -272,3 +271,202 @@ class ModifySteamUserTestCase(TestCase):
         self.assertRedirects(response, f'/accounts/login/?next={url}', status_code=302,
                              target_status_code=200)
 
+
+class ModifyDevelopersTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.developer = Developer.objects.create(
+            name='testdeveloper',
+            creatorUser=self.user
+        )
+
+    def test_modify_developer(self):
+        self.client.login(username='testuser', password='12345')
+
+        url = reverse('modifyDeveloper', kwargs={'id': self.developer.id})
+
+        data = {
+            'name': 'Modified Developer',
+        }
+
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, 302)
+
+        self.developer.refresh_from_db()
+
+        self.assertEqual(self.developer.name, 'Modified Developer')
+
+    def test_developer_modify_invalid_form(self):
+        self.client.login(username='testuser', password='12345')
+        url = reverse('modifyDeveloper', kwargs={'id': self.developer.id})
+        data = {}
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, 200)
+        self.developer.refresh_from_db()
+
+        self.assertEqual(self.developer.name, 'testdeveloper')
+
+    def test_developer_modify_unauthenticated_user(self):
+        url = reverse('modifyDeveloper', kwargs={'id': self.developer.id})
+        data = {}
+        response = self.client.post(url, data)
+        self.assertRedirects(response, f'/accounts/login/?next={url}', status_code=302,
+                             target_status_code=200)
+
+
+class ModifyPublisherTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.publisher = Publisher.objects.create(
+            name='testpublisher',
+            creatorUser=self.user
+        )
+
+    def test_modify_publisher(self):
+        self.client.login(username='testuser', password='12345')
+
+        url = reverse('modifyPublisher', kwargs={'id': self.publisher.id})
+
+        data = {
+            'name': 'Modified Publisher',
+        }
+
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, 302)
+
+        self.publisher.refresh_from_db()
+
+        self.assertEqual(self.publisher.name, 'Modified Publisher')
+
+    def test_publisher_modify_invalid_form(self):
+        self.client.login(username='testuser', password='12345')
+        url = reverse('modifyPublisher', kwargs={'id': self.publisher.id})
+        data = {}
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, 200)
+        self.publisher.refresh_from_db()
+
+        self.assertEqual(self.publisher.name, 'testpublisher')
+
+    def test_publisher_modify_unauthenticated_user(self):
+        url = reverse('modifyDeveloper', kwargs={'id': self.publisher.id})
+        data = {}
+        response = self.client.post(url, data)
+        self.assertRedirects(response, f'/accounts/login/?next={url}', status_code=302,
+                             target_status_code=200)
+
+
+class DeleteProductsTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+
+        self.product = Product.objects.create(
+            name='Test Product',
+            appId=123,
+            type='Test Type',
+            age=18,
+            free=True,
+            recommendations='10',
+            releaseDate='2024-05-17',
+            categories='Test Category',
+            genres='Test Genre',
+            price=19.99,
+            discount=10,
+            languages='English',
+            description='Test Description',
+            creatorUser=self.user
+        )
+
+    def test_product_delete(self):
+        self.client.login(username='testuser', password='12345')
+        url = reverse('deleteProduct', kwargs={'id': self.product.id})
+
+        self.assertTrue(Product.objects.filter(name='Test Product').exists())
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Product.objects.filter(name='Test Product').exists())
+
+    def test_product_delete_unauthenticated_user(self):
+        url = reverse('deleteProduct', kwargs={'id': self.product.id})
+        response = self.client.post(url)
+        self.assertRedirects(response, f'/accounts/login/?next={url}', status_code=302,
+                             target_status_code=200)
+
+
+class DeleteSteamUserTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.steam_user = SteamUser.objects.create(
+            steamID=1234,
+            realName='Test User',
+            personaName='Persona Name',
+            country='Spain',
+            creatorUser=self.user
+        )
+
+    def test_steam_user_delete(self):
+        self.client.login(username='testuser', password='12345')
+        url = reverse('deleteSteamUser', kwargs={'id': self.steam_user.id})
+
+        self.assertTrue(SteamUser.objects.filter(realName='Test User').exists())
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(SteamUser.objects.filter(realName='Test User').exists())
+
+    def test_steam_user_delete_unauthenticated_user(self):
+        url = reverse('deleteSteamUser', kwargs={'id': self.steam_user.id})
+        response = self.client.post(url)
+        self.assertRedirects(response, f'/accounts/login/?next={url}', status_code=302,
+                             target_status_code=200)
+
+
+class DeleteDeveloperTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.developer = Developer.objects.create(
+            name='testdeveloper',
+            creatorUser=self.user
+        )
+
+    def test_developer_delete(self):
+        self.client.login(username='testuser', password='12345')
+        url = reverse('deleteDeveloper', kwargs={'id': self.developer.id})
+
+        self.assertTrue(Developer.objects.filter(name='testdeveloper').exists())
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Developer.objects.filter(name='testdeveloper').exists())
+
+    def test_developer_delete_unauthenticated_user(self):
+        url = reverse('deleteDeveloper', kwargs={'id': self.developer.id})
+        response = self.client.post(url)
+        self.assertRedirects(response, f'/accounts/login/?next={url}', status_code=302,
+                             target_status_code=200)
+
+
+class DeletePublisherTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.publisher = Publisher.objects.create(
+            name='testpublisher',
+            creatorUser=self.user
+        )
+
+    def test_publisher_delete(self):
+        self.client.login(username='testuser', password='12345')
+        url = reverse('deletePublisher', kwargs={'id': self.publisher.id})
+
+        self.assertTrue(Publisher.objects.filter(name='testpublisher').exists())
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Publisher.objects.filter(name='testpublisher').exists())
+
+    def test_publisher_delete_unauthenticated_user(self):
+        url = reverse('deletePublisher', kwargs={'id': self.publisher.id})
+        response = self.client.post(url)
+        self.assertRedirects(response, f'/accounts/login/?next={url}', status_code=302,
+                             target_status_code=200)
